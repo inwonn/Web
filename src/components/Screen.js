0b9beec7e0eab8bf1b996933dd647d7275edd254
inwonn <103662424+inwonn@users.noreply.github.com>
2022-09-02 12:25:41 +0900

    initial commit

---
+++ src/components/Screen.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class Screen extends React.Component
+{
+    constructor(props)
+    {
+        const ref = React.useRef()
+    }
+
+    componentDidMount()
+    {
+
+    }
+    
+    componentDidUpdate()
+    {
+
+    }
+
+    componentWillUnmount()
+    {
+
+    }
+
+    render()
+    {
+        return <div ref={ref}></div>
+    }
+}
